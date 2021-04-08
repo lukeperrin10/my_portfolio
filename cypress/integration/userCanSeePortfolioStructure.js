eb42c207184ea0e25d64bc5e7c630951a4250eca
Luke Perrin <lukeperrin27@gmail.com>
2021-04-08 08:57:30 +0200

    added baseURL , navigating to page in test files now simpler. Updates to User can see portfolio structure and a new file for navigate through page

--- cypress/integration/userCanSeePortfolioStructure.js
+++ cypress/integration/userCanSeePortfolioStructure.js
@@ -1,6 +1,6 @@
 describe("Portfolio interface", () => {
   it("successfully renders", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get("#header").should("contain", "My Portfolio");
     cy.get("#footer").should("contain", "Made with React 17.0.2");
     cy.get("#hello").should("contain", "Hello World");
